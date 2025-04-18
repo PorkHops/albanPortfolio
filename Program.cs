4dd236af1b0ebb715332747c263de2a26f4c61a2
Alban Shekaj <alban.shekaj@gmail.com>
2025-04-18 10:52:52 -0400

    Refactor navigation and layout components for improved readability; update data fetching paths in pages and HTML files for consistency.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "data/") });
 
 await builder.Build().RunAsync();
 
